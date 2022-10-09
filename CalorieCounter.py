625429a9209fd3c57ee03e7750c55459b96bcf4e
Adam Rosas <adamrosas27@gmail.com>
2022-10-09 13:11:58 -0700

    Create constructor method
    Create constructor method that has parameters meal_name. caloric_value, serving_size, number_of_servings

--- CalorieCounter.py
+++ CalorieCounter.py
@@ -1,3 +1,12 @@
 class Calories:
 
     caloric_limit = input("Enter Your Daily Caloric Target: ")
+
+    def __init__(
+        self,
+        meal_name: str,
+        caloric_value: int,
+        serving_size: int,
+        number_of_servings: int,
+    ) -> None:
+        pass
