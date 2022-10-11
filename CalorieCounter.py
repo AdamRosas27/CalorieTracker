51ee030c2fb739ccbdc4bd6deed391006a33cfc7
Adam Rosas <adamrosas27@gmail.com>
2022-10-10 20:30:28 -0700

    Add method
    Add addMeal method that subtracts the calories from food from the caloric limit amount

--- CalorieCounter.py
+++ CalorieCounter.py
@@ -1,10 +1,10 @@
 class Food:
 
     caloric_limit = int(input("Enter Your Daily Caloric Target: "))
-    name = input("Name: ")
-    calories = float(input("Number of calories: "))
-    servings = input("Serving size: ")
-    serving_amount = float(input("Number of servings: "))
+    food_name = input("Name of meal: ")
+    food_calories = float(input("Number of calories: "))
+    food_servings = input("Serving size: ")
+    food_serving_amount = float(input("Number of servings: "))
 
     def __init__(
         self,
@@ -17,3 +17,7 @@ class Food:
         self.caloric_value = caloric_value
         self.serving_size = serving_size
         self.number_of_servings = number_of_servings
+
+    def addMeal(self):
+        self.caloric_limit -= self.caloric_value
+        print(self.caloric_limit)
